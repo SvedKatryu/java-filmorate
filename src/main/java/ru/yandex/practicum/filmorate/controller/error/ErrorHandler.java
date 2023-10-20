1c8a55483cdc1ad0df75f002a46f61485b3a0790
SvedKatryu <ShvedKatryu@yandex.ru>
2023-10-20 17:28:07 +0300

    style: исправлено по Codestyle

--- src/main/java/ru/yandex/practicum/filmorate/controller/error/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/error/ErrorHandler.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
+
 @RestControllerAdvice
 public class ErrorHandler {
 
